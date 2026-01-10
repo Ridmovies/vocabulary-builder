5e559dc8ffe43f3c51c980f527a71a74bd073ec7
riddler_rid <unkownrid@gmail.com>
2026-01-10 10:49:21 +0300

    + add user table

---
+++ app/models/user.py
@@ -0,0 +1,29 @@
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.models import Base
+
+
+class User(Base):
+    """Модель пользователя."""
+    __tablename__ = "users"
+
+    # Основные поля
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        index=True,
+        nullable=False
+    )
+    username: Mapped[str] = mapped_column(
+        String(50),
+        unique=True,
+        index=True,
+        nullable=False
+    )
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    # Активность
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
